6810a74b19e22ea07ced5a49c6d7d758ead7fc83
VinayaKadate <kadatevinayak@gmail.com>
2025-12-18 13:46:38 +0530

    deleted all files

--- prakritipath/src/components/common/Card.jsx
+++
@@ -1,9 +0,0 @@
-const Card = ({ children }) => {
-  return (
-    <div className="bg-white rounded-xl shadow-sm border border-slate-200 p-6">
-      {children}
-    </div>
-  );
-};
-
-export default Card;
